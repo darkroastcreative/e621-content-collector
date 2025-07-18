a94d365c2bbaaa14a2e345d10880d72ec9fd0385
David Urso <david@darkroastcreative.co>
2025-07-17 23:36:32 -0600

    Finished Initial Implementation of Core Download Logic
    This logic will continue to change as the tool is further fleshed out, but it can at least download posts in a simple way!

--- src/e621_content_collector/downloader/download.py
+++ src/e621_content_collector/downloader/download.py
@@ -1,4 +1,5 @@
 import json
+import os
 import requests
 from requests import Response
 
@@ -49,16 +50,20 @@ def read_tag_sets() -> set:
 # TODO: Document!
 # TODO: Consider adding an option where no tag set is specified, which would
 # trigger a download of the latest posts.
+# TODO: Consider adding an option for alternative download location.
 def download(tag_set: str) -> None:
     # Replace spaces in the tag set with the URL encoded version of the space
     # character. While this isn't necessarily necessary, it makes requests to
     # the e621 API more proper.
     tag_set_string: str = tag_set.replace(' ', '%20')
 
-    # Declare and initialize a variable to track which page of posts the
-    # downloader is requesting content from. This is necessary because the e621
-    # API returns content data in pages rather than all at once.
-    page_number = 1
+    # Declare and initialize variable to track which page of posts the
+    # downloader is requesting content from and whether there are more posts to
+    # process. These are necessary because the e621 API returns content data in
+    # pages rather than all at once, and does not indicate whether there is a
+    # "next" page.
+    page_number: int = 1
+    more_posts_available: bool = True
 
     # Decare and initialize a dictionary of headers to include with the content
     # request to e621. This *should* just be the user agent that identifies the
@@ -66,38 +71,69 @@ def download(tag_set: str) -> None:
     # requests).
     headers: dict = {'User-Agent': 'darkroastcreative/e621-content-collector'}
 
-    # Submit a request to the e621 API for posts matching the provided tag set and page number.
-    response: Response = requests.get(url=f'https://e621.net/posts.json?tags={tag_set_string}&page={page_number}', headers=headers)
+    # Process the request, paging through the set of available posts until
+    # either the page limit is reached or the tool detects that there are no
+    # more posts available for the specified tag set.
+    while page_number < 751 and more_posts_available:
+        # Submit a request to the e621 API for posts matching the provided tag set and page number.
+        response: Response = requests.get(url=f'https://e621.net/posts.json?tags={tag_set_string}&page={page_number}', headers=headers)
 
-    # Process the response from the e621 API.
-    if response.status_code == 200:
-        print()
-        # TODO: Implement logic to parse the returned JSON, download posts, and
-        # navigate through pages of posts.
-        # NOTE: The e621 API doesn't provide the number of pages or whether
-        # there is a next page in responses to the API request used. However,
-        # the API will only return up to 750 pages of data. With this in mind,
-        # the way to see if there is more content available to download is to
-        # keep requesting additional pages until either we receive a page with
-        # no posts or we reach the 750th page (whichever comes first).
-        
-        # Parse the response JSON from the e621 API to a Python object for
-        # processing. This line grabs justs the content of the "posts" property
-        # of the response because that's the part needed to parse and download
-        # the posts referenced in the response.
-        posts = json.loads(response.content)['posts']
+        # Process the response from the e621 API.
+        if response.status_code == 200:
+            # Parse the response JSON from the e621 API to a Python object for
+            # processing. This line grabs justs the content of the "posts" property
+            # of the response because that's the part needed to parse and download
+            # the posts referenced in the response.
+            posts = json.loads(response.content)['posts']
 
-        for post in posts:
-            id: int = post['id']
-            url: str = post['file']['url']
-            tags_general = []
-            tags_artist = []
-            tags_contributor = []
-            tags_copyright = []
-            tags_character = []
-            tags_species = []
-            tags_meta = []
-            tags_lore = []
+            if len(posts) > 0:
+                more_posts_available = True
+            else:
+                more_posts_available = False
+
+            # Iterate over the set of posts, extracting post metadata (ID, URL, tag
+            # information) and downloading each post.
+            for post in posts:
+                # Extract post ID. This will be used to name the downloaded file.
+                id: int = post['id']
+
+                # Extract the URL for the source content (image, video, animation,
+                # etc.). The content at this URL will be the content downloaded to
+                # represent the post.
+                url: str = post['file']['url']
+
+                # Extract tag information. This doesn't have an immediate purpose,
+                # as posts returned by the API match the provided tag sets, but
+                # will be used to filter out posts in later versions of this tool. 
+                tags_general = post['tags']['general']
+                tags_artist = post['tags']['artist']
+                tags_contributor = post['tags']['contributor']
+                tags_copyright = post['tags']['copyright']
+                tags_character = post['tags']['character']
+                tags_species = post['tags']['species']
+                tags_meta = post['tags']['meta']
+                tags_lore = post['tags']['lore']
+
+                # Check whether the URL value is null and proceed to download
+                # the post content if not. This logic is present to account for
+                # an oddity with the e621 API in which sometimes posts are
+                # included without URL values.
+                if url is not None:
+                    # Get the file extension for the post. This will be used to
+                    # determine which file extension to use when downloading/saving
+                    # the post content locally.
+                    file_extension: str = url.split('.')[-1]
+
+                    # Get the post data. This will be written to the local machine.
+                    post_data: bytes = requests.get(url).content
+
+                    # Write the post file to the local machine.
+                    with open(os.path.join(os.getcwd(), 'downloads', f'{id}.{file_extension}'), 'wb') as post_file:
+                        post_file.write(post_data)
+
+            # Increment the page number variable so the tool can proceed to
+            # check for the next page of posts.
+            page_number += 1
 
 tag_sets = read_tag_sets()
 
